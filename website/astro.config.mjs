49d4a643d14734f87e97960dac8cb3ca8c8d7bb9
Nitya Narasimhan <nitya@users.noreply.github.com>
2023-07-19 17:17:46 -0400

    Update astro.config.mjs

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   // Added Configuration for 
   // Deployment to GitHub Pages
   site: 'https://30DaysOf.github.io',
-  base: '/sementic-kernel/',
+  base: '/semantic-kernel/',
 
   integrations: [
     starlight({
